e2e4fc3c5517e09741dbfc5cd28c666a75148bc9
believelody <believelody@gmail.com>
2019-04-30 17:32:45 +0200

    Add files via upload

--- src/components/react-motion/list-to-product/Routes.js
+++ src/components/react-motion/list-to-product/Routes.js
@@ -1,1 +1,19 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import asyncComponent from '../asyncComponent.js'
 
+const ReactMotionList = asyncComponent(() => import('./ReactMotionList'))
+const ReactMotionProduct = asyncComponent(() => import('./ReactMotionProduct'))
+
+const DefaultLayout = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={ matchProps => (<Component {...matchProps} />) } />
+)
+
+export default () => {
+  return (
+        <>
+            <DefaultLayout exact path='/react-motion/product-list' component={ReactMotionList} />
+            <DefaultLayout exact path='/react-motion/product-detail' component={ReactMotionProduct} />
+        </>
+  )
+}
